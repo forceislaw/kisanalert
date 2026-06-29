6679e779907299230efae6eaa0167eb860ea1fa2
force <dhirnaman4f1411@gmail.com>
2026-06-29 14:11:55 +0530

    fix: widen chip to 300px so state + temp always fits

--- src/components/map/MapInner.tsx
+++ src/components/map/MapInner.tsx
@@ -83,8 +83,8 @@ function WeatherMarkers({ weather }: { weather: WeatherData[] }) {
             <span style="font-size: 14px; font-weight: 700; margin-left: 4px;">${w.temp}°</span>
           </div>
         `,
-        iconSize: [80, 36],
-        iconAnchor: [40, 18],
+        iconSize: [300, 36],
+        iconAnchor: [150, 18],
       });
 
       return L.marker([w.lat, w.lng], { icon }).addTo(map);
